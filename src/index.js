d2cc1b0c63a6243e667f4857af0d2620b8df118a
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-05-05 00:43:56 -0400

    fixing API to work and updated the CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayStory(response) {
- new Typewriter("#story", {
-    strings: "The women who loved her Golden Retriever so much",
+  new Typewriter("#story", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -8,16 +8,27 @@ function displayStory(response) {
 }
 
 function generateStory(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-let instructionsInput = document.querySelector("#user-story");
-let apiKey = "840df5b063f365oba39tb282d8c1d0cc"
-let content = "you are very smart and have a lot of information and history your mission is to create a short story about the topic the user choose as a topic and follow users instructions";
-let promtp = `Users instructions: Generate a short story about ${instructionsInput.value}`;
-let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}"
+  let instructionsInput = document.querySelector("#user-story");
+  let apiKey = "840df5b063f365oba39tb282d8c1d0cc";
+  let content =
+    "You are very smart and have a lot of information and history your mission is to create a short story about the topic the user choose as a topic and follow users instructions";
+  let promtp = `Users instructions: Generate a short story about ${instructionsInput.value}`;
+  let apiUrl =
+    "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
 
-axios.get(apiUrl).then(displayStory);
-   
+  let storyElement = document.querySelector("#story");
+  storyElement.classList.remove("hidden");
+  storyElement.innerHTML = (
+    <div class="generating">
+      {" "}
+      ⏳ Generating a short story about ${instructionsInput.value}
+    </div>
+  );
+
+  axios.get(apiUrl).then(displayStory);
+}
 
 let storyformGenerator = document.querySelector("story-generator");
-storyformGenerator.addEventListener("submit" generateStory);
+storyformGenerator.addEventListener("submit", generateStory);
