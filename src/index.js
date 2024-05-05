787a8d756403116a43d7eab4814974cfb35bd13d
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-05-05 00:30:27 -0400

    added API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateStory(event) {
-    event.preventDefault();
-    new Typewriter("#story", {
+function displayStory(response) {
+ new Typewriter("#story", {
     strings: "The women who loved her Golden Retriever so much",
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+
+function generateStory(event) {
+    event.preventDefault();
+
+let instructionsInput = document.querySelector("#user-story");
+let apiKey = "840df5b063f365oba39tb282d8c1d0cc"
+let content = "you are very smart and have a lot of information and history your mission is to create a short story about the topic the user choose as a topic and follow users instructions";
+let promtp = `Users instructions: Generate a short story about ${instructionsInput.value}`;
+let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}"
+
+axios.get(apiUrl).then(displayStory);
+   
 
 let storyformGenerator = document.querySelector("story-generator");
 storyformGenerator.addEventListener("submit" generateStory);
